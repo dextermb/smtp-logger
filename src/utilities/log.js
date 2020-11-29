14bbf6d9aec2d8c7bd4f204490b605380dcb372f
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2020-11-29 10:30:10 +0000

    Add timestamps to file log

--- src/utilities/log.js
+++ src/utilities/log.js
@@ -5,8 +5,16 @@ const handler = (
   winston.createLogger({
     level: 'silly',
     transports: [
-      new winston.transports.Console({ format: winston.format.simple() }),
-      new winston.transports.File({ filename: constants.PATH.STORAGE.LOGS })
+      new winston.transports.Console({
+        format: winston.format.simple()
+      }),
+      new winston.transports.File({
+        filename: constants.PATH.STORAGE.LOGS,
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.json()
+        )
+      })
     ]
   })
 )
